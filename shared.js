e003826df4a13bb0d42c257f892f0ad1549b3492
ben lang <9093983+bl3rune@users.noreply.github.com>
2024-12-09 01:57:48 +0000

    V3 - Rework Nav / Make site mobile responsive / Addd vimeo links

---
+++ shared.js
@@ -0,0 +1,9 @@
+
+function menuControl() {
+  var x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}

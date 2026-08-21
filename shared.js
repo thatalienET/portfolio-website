147d05b89488c21b940f6a8e8c1fa831990b8c6d
bl3rune <bl3rune@github.com>
2026-08-21 22:14:31 +0100

    Swap to nunjucks templating and redesign

--- shared.js
+++
@@ -1,9 +0,0 @@
-
-function menuControl() {
-  var x = document.getElementById("topnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
